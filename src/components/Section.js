bce4229feb677b334bf0c3f68b869d20f2a33198
Aain Ur Rehman <aain134@gmail.com>
2023-12-13 13:16:18 +0500

    Add a section Task

---
+++ src/components/Section.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Section.css'
+function Section() {
+  return (
+    <main className='main-container'>
+    <div className='Section-container'>
+      <div className='text-container'>
+        <div className='text'>
+        <span className='text-child' > WELCOME TO CODESHAPER</span>
+        </div>
+        <h1 className='text-2'>CONSULTING<br/>FOR EVERY <br/>BUSINESS</h1>
+        
+ <div style={{ transitionDelay: '500ms', marginTop: '50px' }}>
+
+    <button className='btn' type='submitt'>Dicover More</button>
+ </div>
+      </div>
+
+    </div>
+  </main>
+
+  )
+}
+
+export default Section
